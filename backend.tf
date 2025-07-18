86e8bbef2c15221d26f7a353d8844cbdfc4ab656
Oluwatobi Solarin <solatobi001@gmail.com>
2025-07-18 09:57:59 -0500

    work update

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
-# Statefile
-# terraform {
-#   backend "s3" {
-#     encrypt = true
-#     bucket  = "tobi-bucket243-lockfile"
-#     key     = "backend.tf"
-#     region  = "us-east-1"
-#     dynamodb_table = "terraformlocks"
-#   }
-# }
+
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket  = "bucket345-lock00202"
+    key     = "backend.tf"
+    region  = "us-east-1"
+    dynamodb_table = "projectlocktable"
+  }
+}
 
